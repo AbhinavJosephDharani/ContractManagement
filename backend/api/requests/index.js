deffe5dd980fd6c8a25915cb5f70253076851e63
AbhinavJosephDharani <abhinav.dharani7@gmail.com>
2025-12-07 01:14:27 -0500

    fix: add Authorization header to CORS headers on all endpoints

--- backend/api/requests/index.js
+++ backend/api/requests/index.js
@@ -6,7 +6,7 @@ module.exports = async function handler(req, res) {
   // CORS
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   if (req.method === 'OPTIONS') return res.status(204).end()
 
   let client
